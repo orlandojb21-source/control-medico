2207fc50d037aa5ca84141b6ce633f41a4e8873b
orlandojb21-source <orlandojb.21@gmail.com>
2026-05-28 14:51:40 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE_NAME = 'control-medico-v1';
+var urlsToCache = [
+  '/control-medico/',
+  '/control-medico/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  // No cachear llamadas al API de Google
+  if (event.request.url.includes('script.google.com')) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+});
